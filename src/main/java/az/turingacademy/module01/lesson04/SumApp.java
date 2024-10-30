775bddc626602cd6ff95a7d4ab1198e4b65ce7a5
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2024-10-30 16:39:09 +0400

    backend repository structure attached

--- src/main/java/az/turingacademy/module01/lesson04/SumApp.java
+++ src/main/java/az/turingacademy/module01/lesson04/SumApp.java
@@ -5,9 +5,9 @@ public class SumApp {
         short engine = 1945;
         int fuel = 5;
 
-        int  turbo = engine * fuel;
+        int turbo = engine * fuel;
         int filter = engine / fuel;
-        int door  =  engine % fuel;
+        int door = engine % fuel;
         int window = engine + fuel;
         int piston = engine - fuel;
 
@@ -21,14 +21,30 @@ public class SumApp {
         int y = 20;
         int z = 30;
         int w = 40;
+        int t = 50;
+        int r = 50;
+        int g = 50;
+
         x++;
         y--;
-        z-=4;
-        w-=5;
+        z -= 4;
+        w -= 5;
+        t *= 5;
+        boolean b = r != 5;
+        boolean b1 = g >= 40;
+
+
+
 
         System.out.println(x);
         System.out.println(y);
         System.out.println(z);
         System.out.println(w);
+        System.out.println(t);
+        System.out.println(b);
+        System.out.println(b1);
+
+
+
     }
 }
