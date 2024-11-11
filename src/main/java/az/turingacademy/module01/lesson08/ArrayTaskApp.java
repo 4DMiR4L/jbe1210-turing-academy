9a5249c576d4c6e0796e58b66fa767a5308d37ce
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2024-11-11 19:23:22 +0400

    master

--- src/main/java/az/turingacademy/module01/lesson08/ArrayTaskApp.java
+++ src/main/java/az/turingacademy/module01/lesson08/ArrayTaskApp.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class ArrayTaskApp {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter: ");
+
         int n = scanner.nextInt();
         int[][] array = new int[n][];
+
         for (int i = 1; i <= n; i++) {
             array[i - 1] = new int[i];
             for (int j = 0; j < i; j++) {
-                array[i-1][j] = i;
-        }
+                array[i - 1][j] = i;
+            }
         }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
