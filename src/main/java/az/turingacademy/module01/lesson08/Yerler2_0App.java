18d3ba0a83773f87602cd1eed7a566a1c28aeaa6
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2024-11-17 14:22:10 +0400

    master

--- src/main/java/az/turingacademy/module01/lesson08/Yerler2_0App.java
+++ src/main/java/az/turingacademy/module01/lesson08/Yerler2_0App.java
@@ -7,13 +7,14 @@ public class Yerler2_0App {
     public static void main(String[] args) {
         Random random = new Random();
 
-        String[] adlar = {"Rəvan Agayev", "Gülər Əbilova", "Yetər Nəbiyeva",
+        String[] adlar = {
+                "Rəvan Agayev", "Gülər Əbilova", "Yetər Nəbiyeva",
                 "Ruslan Çərkəzov", "Sənubər Abbasova", "Fəxri Babayev",
-                "Lalə Həmidova", "Günay İskəndərova","Mirzəyeva Dilbər",
-                "Ramina Səlimova", "Tural Nəzərov", "Rəvan Əliyev", "İbrahim Ələkbərov",
+                "Lalə Həmidova", "Günay İskəndərova", "Mirzəyeva Dilbər",
+                "Ramina Səlimova", "Tural Nəzərov", "Rəvan Əliyev",
                 "Aynur Məmmədova", "Ömər Ələkbərov", "Famil Məmmədov",
-                "Rena Mehdiyeva", "ƏliMurad", "Seyran Xəlilzadə",
-                "Hüseynov Məhəmməd", "Pərvin Bədəlova", "Nicat Məcidov","Ella Piriyeva",};
+                "Rena Mehdiyeva", "ƏliMurad", "Seyran Xəlilzadə", "İbrahim Ələkbərov",
+                "Hüseynov Məhəmməd", "Pərvin Bədəlova", "Nicat Məcidov", "Ella Piriyeva",};
         System.out.println("adlar ve yerler");
         boolean[] istifadeolunan = new boolean[adlar.length];
 
