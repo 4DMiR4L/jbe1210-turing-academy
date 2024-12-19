874526c43dfe22db0ce5c513b541bbafc07a05b2
LalaHamidova1210 <lalahamidova1210@gmail.com>
2024-12-19 20:44:28 +0400

    man and woman added

---
+++ src/main/java/az/turingacademy/module02/familyApp/Man.java
@@ -0,0 +1,14 @@
+package az.turingacademy.module02.familyApp;
+
+public class Man extends Human {
+
+    public Man(String name, String surname, int dateOfBirth, int IQ, Pet pet, Family family, String[][] shedule) {
+        super(name, surname, dateOfBirth, IQ, pet, family, shedule);
+    }
+    public void greetPet () {
+    }
+    public void repairCar () {
+
+    }
+
+}
---
+++ src/main/java/az/turingacademy/module02/familyApp/Woman.java
@@ -0,0 +1,14 @@
+package az.turingacademy.module02.familyApp;
+
+public class Woman extends Human {
+    public Woman(String name, String surname, int dateOfBirth, int IQ, Pet pet, Family family, String[][] shedule) {
+        super(name, surname, dateOfBirth, IQ, pet, family, shedule);
+    }
+     public void greetPet () {
+    }
+    public void Makeup (){
+
+    }
+
+
+}
