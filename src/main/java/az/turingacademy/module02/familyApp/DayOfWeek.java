20813cbec8aedc8730593e43ef02d8c27493acde
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-01-09 17:55:22 +0400

    master

--- src/main/java/az/turingacademy/module02/familyApp/DayOfWeek.java
+++
@@ -1,5 +0,0 @@
-package az.turingacademy.module02.familyApp;
-
-public enum DayOfWeek {
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
-}
--- src/main/java/az/turingacademy/module02/familyApp/Foul.java
+++
@@ -1,7 +0,0 @@
-package az.turingacademy.module02.familyApp;
-
-public interface Foul {
-
-    public default void respond() {
-    }
-}
