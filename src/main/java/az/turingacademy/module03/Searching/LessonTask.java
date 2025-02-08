e4060b46acc3b1aeac164dc9bee681145db3a881
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-02-08 13:08:21 +0400

    master

---
+++ src/main/java/az/turingacademy/module03/Searching/LessonTask.java
@@ -0,0 +1,43 @@
+package az.turingacademy.module03.Searching;
+
+import java.util.*;
+
+public class LessonTask {
+    public static void main(String[] args) {
+
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for (int i = 1; i <= 20; i++) {
+//            list.add(i);
+//        }
+//        list.stream().filter(integer -> integer % 2 == 0).map(integer -> integer * 2).forEach(System.out::println);
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("faxı");
+        linkedList.add("ibrahim");
+        linkedList.add("lale");
+        linkedList.add("yeter");
+        linkedList.add("senuber");
+        linkedList.add("seyran");
+        linkedList.add("aynur");
+        linkedList.add("rena");
+        linkedList.add("diber");
+        linkedList.add("ali");
+
+        LinkedList<String> linkedList2 = new LinkedList<>();
+        linkedList.stream().filter(String ->String.startsWith("a") || String.length()>5).forEach(linkedList2::add);
+        System.out.println(linkedList2);
+
+        Set<String> set = new HashSet<>();
+        set.addAll(linkedList2);
+        System.out.println(set);
+
+        Queue<String> queue = new LinkedList<>();
+        queue.addAll(set);
+        System.out.println(queue);
+
+        Stack<String> stack = new Stack<>();
+        stack.addAll(queue);
+        System.out.println(stack);
+
+    }
+}
