fb63957df6953037874d4e185c9243273958d71f
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-03-01 18:40:48 +0400

    master

--- src/main/java/az/turingacademy/module03/Searching/StreamApp.java
+++ src/main/java/az/turingacademy/module03/Searching/StreamApp.java
@@ -3,7 +3,6 @@ package az.turingacademy.module03.Searching;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamApp {
     public static void main(String[] args) {
@@ -22,10 +21,10 @@ public class StreamApp {
         integerList.add(4);
         integerList.add(5);
 
-//        stringlist.stream().filter(string -> string.length() > 3).forEach(string -> System.out.println(string));
-//        stringlist.stream().filter(h1 ->h1.startsWith("A")).forEach(string -> System.out.println(string));
-//        stringlist.stream().sorted((Comparator.naturalOrder())).forEach(string -> System.out.println(string));
-//        integerList.stream().filter(integer -> integer > 3).forEach(integer -> System.out.println(integer));
+        stringlist.stream().filter(string -> string.length() > 3).forEach(string -> System.out.println(string));
+        stringlist.stream().filter(h1 ->h1.startsWith("A")).forEach(string -> System.out.println(string));
+        stringlist.stream().sorted((Comparator.naturalOrder())).forEach(string -> System.out.println(string));
+        integerList.stream().filter(integer -> integer > 3).forEach(integer -> System.out.println(integer));
         integerList.stream().filter(integer -> integer.compareTo(4)<=0).forEach(integer -> System.out.println(integer));
     }
 }
